2e13a7aaed5d939dd68212c3b6bba5f48f4ac218
nikolaokonesh <nikolaokonesh@gmail.com>
2020-02-14 21:38:08 +1100

    add projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,45 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
+  load_and_authorize_resource :except => [:show]
+
+  def show
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def edit
+  end
+
+  def create
+    @project = Project.new(partner_params)
+
+    if @project.save
+      redirect_to root_url(anchor: 'partners')
+    end
+  end
+
+  def update
+    if @project.update(partner_params)
+      redirect_to root_url(anchor: 'partners'), notice: 'Post was successfully updated.'
+    end
+  end
+
+  def destroy
+    @project.destroy
+    redirect_to root_url(anchor: 'partners'), notice: 'Post was successfully destroyed.'
+  end
+
+  private
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_project
+      @project = Project.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def partner_params
+      params.require(:project).permit(:title, :body, :photo, :description)
+    end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,14 @@
 class Project < ApplicationRecord
   validates :title, presence: true, length: { minimum: 3, maximum: 400 }
-  validates :url, url: { schemes: ['https'] }
+  validates :url, url: { schemes: ['http', 'https'] }
   has_rich_text :body
   validates_presence_of :body
   validates :body, length: { maximum: 32_000 }
+  has_one_attached :photo
+  validates :photo, blob: { content_type: :image, size_range: 1..5.megabytes }
+  after_create :descript_create
+  def descript_create
+    update_attribute(:description, body.to_plain_text.to_s[0..200])
+    save!
+  end
 end
